ad0e1edc266125542ae5eb73ec19f8d44a525ceb
Chukwuma Anyadike <chukwuma.anyadike@gmail.com>
2025-05-17 09:01:35 -0500

    Dark mode added

--- src/app/component/nav-container/nav-container.component.ts
+++ src/app/component/nav-container/nav-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, HostBinding, inject } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { AsyncPipe } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -10,6 +10,8 @@ import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-nav-container',
@@ -24,7 +26,8 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
     AsyncPipe,
     RouterOutlet,
     RouterLink,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    ReactiveFormsModule
   ]
 })
 export class NavContainerComponent {
@@ -35,4 +38,24 @@ export class NavContainerComponent {
       map(result => result.matches),
       shareReplay()
     );
+
+  switchTheme = new FormControl(false);
+  @HostBinding('class') className = '';
+  darkClass = 'dark-mode';
+  lightClass = 'light-mode';
+
+  constructor(private overlay: OverlayContainer) { }
+
+  ngOnInit() {
+    this.switchTheme.valueChanges.subscribe((isDarkMode) => {
+      this.className = isDarkMode ? this.darkClass : this.lightClass;
+
+      const bodyClass = document.body.classList;
+      if (isDarkMode) {
+        bodyClass.add(this.darkClass);
+      } else {
+        bodyClass.remove(this.darkClass);
+      }
+    });
+  }
 }
