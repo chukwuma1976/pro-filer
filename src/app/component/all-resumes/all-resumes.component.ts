e96689adf257d0b76965c7511a8b38fd34431003
Chukwuma Anyadike <chukwuma.anyadike@gmail.com>
2025-08-07 22:51:58 -0500

    Fixed sorting and pagination for dynamic data, also made style changes

--- src/app/component/all-resumes/all-resumes.component.ts
+++ src/app/component/all-resumes/all-resumes.component.ts
@@ -9,7 +9,7 @@ import { FormControl } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
-import { MatSort, Sort, MatSortModule } from '@angular/material/sort';
+import { MatSort, MatSortModule } from '@angular/material/sort';
 import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -47,8 +47,18 @@ export class AllResumesComponent {
   viewResumeMessage = 'View the details of this resume.';
   isLoading = true; // Flag to indicate loading state
 
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatPaginator, { static: false }) //set paginator for dynamically loaded data
+  set paginator(value: MatPaginator) {
+    if (this.dataSource) {
+      this.dataSource.paginator = value;
+    }
+  }
+  @ViewChild(MatSort, { static: false }) //set sort for dynamically loaded data
+  set sort(value: MatSort) {
+    if (this.dataSource) {
+      this.dataSource.sort = value;
+    }
+  }
 
   constructor(private resumeService: ResumeService) { }
 
@@ -65,17 +75,12 @@ export class AllResumesComponent {
     this.dataSource.sort = this.sort;
   }
 
-  /** Announce the change in sort state for assistive technology. */
-  announceSortChange(sortState: Sort) {
-    if (sortState.direction) {
-      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
-    } else {
-      this._liveAnnouncer.announce('Sorting cleared');
-    }
-  }
-
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
