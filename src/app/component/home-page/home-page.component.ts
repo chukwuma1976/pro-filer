0d2419570474cdbd6b748fabcfe4db78a1aec771
Chukwuma Anyadike <chukwuma.anyadike@gmail.com>
2025-09-10 21:53:40 -0500

    Updated home page

--- src/app/component/home-page/home-page.component.ts
+++ src/app/component/home-page/home-page.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { PreviewResumeComponent } from '../preview-resume/preview-resume.component';
 import { ResumeDetailsComponent } from '../resume-details/resume-details.component';
 import { trigger, transition, style, animate, query, stagger } from '@angular/animations';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { TEMPLATES } from '../../shared/constants';
 import { delay } from 'rxjs';
 import sampleResume from '../../shared/mock-data/sample-resume.json'
 import blankResume from '../../shared/mock-data/blank-resume.json'
@@ -10,25 +11,28 @@ import { Resume } from '../../shared/models/resume';
 
 @Component({
   selector: 'app-home-page',
-  imports: [PreviewResumeComponent, ResumeDetailsComponent],
+  imports: [PreviewResumeComponent, ResumeDetailsComponent, MatButtonModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
-  animations: [
-    trigger('staggeredEnter', [
-      transition(':enter', [
-        query('input', [
-          style({ opacity: 0, transform: 'translateY(-10px)' }),
-          stagger('200ms', [
-            animate('300ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
-          ])
-        ], { optional: true })
-      ])
-    ])
-  ]
+  // animations: [
+  //   trigger('staggeredEnter', [
+  //     transition(':enter', [
+  //       query('input', [
+  //         style({ opacity: 0, transform: 'translateY(-10px)' }),
+  //         stagger('200ms', [
+  //           animate('300ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
+  //         ])
+  //       ], { optional: true })
+  //     ])
+  //   ])
+  // ]
 })
 export class HomePageComponent {
   formData: Resume = sampleResume;
   dynamicFormData: Resume = { ...blankResume };
+  typingSpeed = 100;  //Milliseconds per character
+  templates = TEMPLATES;
+  buttonsDisabled = false;
 
   constructor() { }
 
@@ -36,39 +40,74 @@ export class HomePageComponent {
     this.simulateFormFill();
   }
 
+  demoTemplate(template: any) {
+    this.dynamicFormData = { ...blankResume };
+    this.dynamicFormData["template"] = template.value;
+    this.simulateFormFill();
+  }
+
   simulateFormFill() {
+    console.log(this.buttonsDisabled);
     const fields: string[] = Object.keys(this.formData);
     const objectFields = [
       "experience",
       "education",
+    ];
+    const arrayFields = [
       "skills",
       "certifications",
       "projects",
       "publications",
       "volunteerExperience"
-    ]
+    ];
     const nonTextFields = [
       "id",
       "shareWithOthers",
       "template",
       "userId"
-    ]
+    ];
     let delayAmount = 500;
 
     fields.forEach((field: string) => {
-      if (!(objectFields.includes(field) || nonTextFields.includes(field))) {
-        console.log('typing called: ', field);
+      if (!(objectFields.includes(field) || arrayFields.includes(field) || nonTextFields.includes(field))) {
         const value = this.formData[field as keyof Resume];
         const text: string = value !== undefined ? String(value) : '';
         this.typeAnimation(field, text);
+      }
+      if (arrayFields.includes(field)) {
+        const values = this.formData[field as keyof Resume];
+        if (Array.isArray(values)) {
+          (values as string[]).forEach((value: string, index: number) => {
+            this.typeAnimationArray(field, index, value);
+          });
+        }
+      }
+      if (objectFields.includes(field)) {
+        const arrays = ['description', 'awards'];
+        const value = this.formData[field as keyof Resume];
+        if (Array.isArray(value) && value.length > 0) {
+          value.forEach((obj: any, index1: number) => {
+            if (typeof obj === 'object' && obj !== null) {
+              Object.keys(obj).forEach((val: string) => {
+                if (!arrays.includes(val)) {
+                  const text = (obj as Record<string, any>)[val];
+                  this.typeAnimationObject(field, val, text);
+                } else {
+                  const elements = (obj as Record<string, any>)[val];
+                  (elements as string[]).forEach((text: string, index: number) => {
+                    this.typeAnimationObjectArray(field, index1, val, index, text);
+                  });
+                }
+              });
+            }
+          })
+        }
       }
     });
   }
 
   typeAnimation(fieldName: string, text: string) {
     let currentIndex = 0;
-    const typingSpeed = 50 //Milliseconds per character
-    const initialDelay = text.length * typingSpeed; //total time to type text in milliseconds
 
     const intervalId = setInterval(() => {
       if (currentIndex <= text.length) {
@@ -78,6 +117,48 @@ export class HomePageComponent {
       } else {
         clearInterval(intervalId);
       }
-    }, typingSpeed);
+    }, this.typingSpeed);
+  }
+
+  typeAnimationArray(fieldName: string, index: number, text: string) {
+    let currentIndex = 0;
+
+    const intervalId = setInterval(() => {
+      if (currentIndex <= text.length) {
+        const partialText = text.substring(0, currentIndex);
+        (this.dynamicFormData as any)[fieldName][index] = partialText;
+        currentIndex++;
+      } else {
+        clearInterval(intervalId);
+      }
+    }, this.typingSpeed);
+  }
+
+  typeAnimationObject(fieldName: string, property: string, text: string) {
+    let currentIndex = 0;
+
+    const intervalId = setInterval(() => {
+      if (currentIndex <= text.length) {
+        const partialText = text.substring(0, currentIndex);
+        (this.dynamicFormData as any)[fieldName][0][property] = partialText;
+        currentIndex++;
+      } else {
+        clearInterval(intervalId);
+      }
+    }, this.typingSpeed);
+  }
+
+  typeAnimationObjectArray(fieldName: string, index1: number, property: string, index2: number, text: string) {
+    let currentIndex = 0;
+
+    const intervalId = setInterval(() => {
+      if (currentIndex <= text.length) {
+        const partialText = text.substring(0, currentIndex);
+        (this.dynamicFormData as any)[fieldName][index1][property][index2] = partialText;
+        currentIndex++;
+      } else {
+        clearInterval(intervalId);
+      }
+    }, this.typingSpeed);
   }
 }
